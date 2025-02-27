7f05b04a7769c94edac895ff13197bab3ad6b5d8
kingilsildor <tychostam@gmail.com>
2025-02-27 09:32:10 +0100

    class based approach:

--- modules/DLA_model.py
+++ modules/DLA_model.py
@@ -1,62 +1,31 @@
-import matplotlib.pyplot as plt
 import numpy as np
+from typing import Tuple, List, Set
 
+CLUSTER_VALUE = 0
 
-def create_grid(N: int) -> np.ndarray:
-    """
-    Create a grid of size N x N with zeros.
+class Diffusion:
+    def __init__(self, N: int, initial_point: str = 'bottom'):
+        self.N = N
+        self.grid = np.zeros((N, N))
+        assert self.grid.shape == (N, N)
 
-    Params
-    ------
-    - N (int): size of the grid
+        if initial_point == 'bottom':
+            coords = (0, N // 2)
+            self.grid[coords] = CLUSTER_VALUE
+        elif initial_point == 'top':
+            coords = (N - 1, N // 2)
+            self.grid[coords] = CLUSTER_VALUE
+        elif initial_point == 'center':
+            coords = (N // 2, N // 2)
+            self.grid[coords] = CLUSTER_VALUE
+        else:
+            raise ValueError("Invalid initial point, choose from 'bottom', 'top' or 'center'")
+        
+        assert np.sum(self.grid) == CLUSTER_VALUE
 
-    Returns
-    -------
-    - grid (np.ndarray): grid of size N x N with zeros
-    """
-    grid = np.zeros((N, N))
-    assert grid.shape == (N, N)
-    return grid
+        self.cluster: Set[Tuple[int, int]] = set()
 
+    def add_to_cluster(self, coords: Tuple[int, int]):
+        self.cluster.add(coords)
+    
 
-def init_grid(grid: np.ndarray, initial_point: str = "bottom") -> np.ndarray:
-    """
-    Initialize the grid with a single particle at the center of the grid.
-
-    Params
-    ------
-    - grid (np.ndarray): grid of size N x N
-    - initial_point (str): initial point of the particle, can be "bottom", "center" or "top". Default is "bottom"
-
-    Returns
-    -------
-    - grid (np.ndarray): grid of size N x N with a single particle at the initial point
-    """
-    grid = grid.copy()
-    assert grid.shape[0] == grid.shape[1]
-    N = grid.shape[0]
-
-    if initial_point == "bottom":
-        grid[N - 1, N // 2] = 2
-    elif initial_point == "center":
-        grid[N // 2, N // 2] = 2
-    elif initial_point == "top":
-        grid[0, N // 2] = 2
-    else:
-        ValueError("Invalid initial point. Must be 'bottom', 'center' or 'top'.")
-
-    assert grid.shape == (N, N)
-    assert np.sum(grid == 2) == 1
-    return grid
-
-
-def plot_grid(grid: np.ndarray):
-    plt.imshow(grid, interpolation="nearest")
-    plt.show()
-
-
-if __name__ == "__main__":
-    N = 100
-    grid = create_grid(N)
-    grid = init_grid(grid)
-    plot_grid(grid)
